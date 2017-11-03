dd50456f38bd234e5940c0a2bb4391aa64915903
GetSubhan <getsubhankhan.1@gmail.com>
2017-11-03 12:41:00 +0530

    this is now done

---
+++ Day1.py
@@ -0,0 +1,13 @@
+class Subhan:
+
+    age = 25
+    cloth = "blah blah"
+    def milan(self):
+        print("ham daku hai goli khaoge")
+a = Subhan()
+print (Subhan.age)
+print(a.age)
+# print(a.age)
+print(a.age)
+# print(a.age)
+#
